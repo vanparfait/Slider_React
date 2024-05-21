9593bf6dd875bd4b9e66aa07e1a27a9435f580f0
vanparfait <nguemechiaparfait@gmail.com>
2024-05-21 13:01:00 +0100

    composant slider ajoute

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Slider from "./components/Slider/Slider";
+
+function App() {
+  return (
+    <>
+      <h1>
+        <Slider />
+      </h1>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "../Slider/Slider.css";
+import leftChevron from "../../assets/left-arrow.svg";
+import rightChevron from "../../assets/right-arrow.svg";
+
+const Slider = () => {
+  return (
+    <>
+      <p className="index-info">3 / 5</p>
+      <div className="slider">
+        <p className="image-info">Bedroom</p>
+        <img src="./images/img-3.jpg" alt="gggggg" className="slider-img" />
+        <button className="navigation-button prev-button">
+          <img src={leftChevron} alt="previous-image" />
+        </button>
+        <button className="navigation-button prev-button">
+          <img src={rightChevron} alt="next-image" />
+        </button>
+      </div>
+    </>
+  );
+};
+
+export default Slider;
