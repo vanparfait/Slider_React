cb3d52a6af0c4d0a1b3df3f1f120b21024fec462
vanparfait <nguemechiaparfait@gmail.com>
2024-05-22 08:16:10 +0100

    gestion du state pour gerer le deplacement des images

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,19 +1,51 @@
-import React from "react";
+import React, { useState } from "react";
+import sliderData from "../../data/sliderData";
 import "../Slider/Slider.css";
 import leftChevron from "../../assets/left-arrow.svg";
 import rightChevron from "../../assets/right-arrow.svg";
 
 const Slider = () => {
+  const [sliderIndex, setSliderIndex] = useState(1);
+
+  function nextSlide(newIndex) {
+    setSliderIndex(sliderIndex + 1);
+    if (sliderIndex > sliderData.length) {
+      setSliderIndex(1);
+    }
+  }
+
+  function previousSlide() {
+    setSliderIndex(sliderIndex - 1);
+  }
+  if (sliderIndex < 1) {
+    setSliderIndex(sliderData.length);
+  }
+
   return (
     <>
-      <p className="index-info">3 / 5</p>
+      <p className="index-info">
+        {sliderIndex} / {sliderData.length}
+      </p>
       <div className="slider">
-        <p className="image-info">Bedroom</p>
-        <img src="./images/img-3.jpg" alt="gggggg" className="slider-img" />
-        <button className="navigation-button prev-button">
+        <p className="image-info">
+          {" "}
+          {sliderData.find((obj) => obj.id === sliderIndex).description}{" "}
+        </p>
+        <img
+          src={`./images/img-${sliderIndex}.jpg`}
+          alt="gggggg"
+          className="slider-img"
+        />
+        <button
+          onClick={() => previousSlide()}
+          className="navigation-button prev-button"
+        >
           <img src={leftChevron} alt="previous-image" />
         </button>
-        <button className="navigation-button next-button">
+        <button
+          onClick={() => nextSlide()}
+          className="navigation-button next-button"
+        >
           <img src={rightChevron} alt="next-image" />
         </button>
       </div>
